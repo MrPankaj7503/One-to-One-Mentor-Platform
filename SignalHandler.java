ce5eec072211bddd901df2c50633d0d164537c24
Pankaj Kumar Mahato <pankajkumarmahato011@gmail.com>
2026-04-03 19:32:54 +0530

    Add files via upload

---
+++ SignalHandler.java
@@ -0,0 +1,30 @@
+package com.pankaj.mentor.config;
+
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.*;
+
+public class SignalHandler extends TextWebSocketHandler {
+
+    private static final Set<WebSocketSession> sessions = new HashSet<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession s : sessions) {
+            if (s.isOpen() && s != session) {
+                s.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        sessions.remove(session);
+    }
+}
